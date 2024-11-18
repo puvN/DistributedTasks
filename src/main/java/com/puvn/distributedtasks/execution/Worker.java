d118482dbe5912d75636813e568371ddfa4484ad
EvLukin <evgeniy.a.lukin@dxc.com>
2024-11-18 17:20:52 +0100

    renamed worker name field

--- src/main/java/com/puvn/distributedtasks/execution/Worker.java
+++ src/main/java/com/puvn/distributedtasks/execution/Worker.java
@@ -11,14 +11,14 @@ import java.util.concurrent.BlockingQueue;
 @Slf4j
 public class Worker extends Thread {
     @Getter
-    private final String name;
+    private final String workerName;
     @Getter
     private final ThreadLocal<WorkerStatus> status = new ThreadLocal<>();
     private final BlockingQueue<Task> taskQueue;
     private final TaskManager taskManager;
 
     public Worker(String name, BlockingQueue<Task> taskQueue, TaskManager taskManager) {
-        this.name = name;
+        this.workerName = name;
         this.taskQueue = taskQueue;
         this.taskManager = taskManager;
         status.set(WorkerStatus.INITIALIZED);
@@ -33,11 +33,11 @@ public class Worker extends Thread {
                 status.set(WorkerStatus.TASK_ASSIGNED);
                 updateTask(task, TaskStatus.EXECUTING);
                 try {
-                    log.info("Executing {} on Worker {}", task.getTaskName(), this.name);
+                    log.info("Executing {} on Worker {}", task.getTaskName(), this.workerName);
                     status.set(WorkerStatus.BUSY);
                     task.run();
                     updateTask(task, TaskStatus.COMPLETED);
-                    log.info("Completed {} on Worker {}", task.getTaskName(), this.name);
+                    log.info("Completed {} on Worker {}", task.getTaskName(), this.workerName);
                 } catch (Exception e) {
                     log.error("Error with task {}, {}", task.getTaskName(), e.getMessage());
                     updateTask(task, TaskStatus.ERROR);
@@ -45,7 +45,7 @@ public class Worker extends Thread {
                     status.set(WorkerStatus.AVAILABLE);
                 }
             } catch (InterruptedException e) {
-                log.error("Fatal error, could not take message from queue, interrupting {}", this.name, e);
+                log.error("Fatal error, could not take message from queue, interrupting {}", this.workerName, e);
                 Thread.currentThread().interrupt();
                 status.set(WorkerStatus.FIRING);
                 break;
